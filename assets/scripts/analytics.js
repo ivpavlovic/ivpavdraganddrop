0560186fc8374bd150d8000c12f1731ab4179c58
ivpavlovic <40268644+ivpavlovic@users.noreply.github.com>
2026-08-03 15:34:59 +0200

    Prva verzija projekta

---
+++ assets/scripts/analytics.js
@@ -0,0 +1,7 @@
+let intervalId = setInterval(() => {
+  console.log('Sending analytics data...');
+}, 2000);
+
+document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+  clearInterval(intervalId);
+});
